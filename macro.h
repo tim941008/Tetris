e887f24b67fe48e0851b0db1d7cd440276f00faa
tim941008 <t2005941008@gmail.com>
2025-12-03 20:52:45 +0800

    update

--- macro.h
+++ macro.h
@@ -1,8 +1,8 @@
 ; =====================================================================
-; macro.h - �Xù����������w
+; macro.h - 俄羅斯方塊巨集庫
 ; =====================================================================
 
-; �]�w�� VGA 640x480 16��Ҧ� (Mode 12h)
+; 設定為 VGA 640x480 16色模式 (Mode 12h)
 INIT_GRAPHICS_MODE MACRO
     push ax
     mov ax, 0012h
@@ -10,7 +10,7 @@ INIT_GRAPHICS_MODE MACRO
     pop ax
 ENDM
 
-; �]�w����r�Ҧ� (�h�X�C����)
+; 設定為文字模式 (退出遊戲用)
 EXIT_TEXT_MODE MACRO
     push ax
     mov ax, 0003h
@@ -18,9 +18,9 @@ EXIT_TEXT_MODE MACRO
     pop ax
 ENDM
 
-; �e�@���I (Pixel)
-; �Ѽ�: X_POS (�Ȧs���α`��), Y_POS (�Ȧs���α`��), COLOR (�Ȧs���α`��)
-; �ϥ� INT 10h, AH=0Ch
+; 畫一個點 (Pixel)
+; 參數: X_POS (暫存器或常數), Y_POS (暫存器或常數), COLOR (暫存器或常數)
+; 使用 INT 10h, AH=0Ch
 DRAW_PIXEL MACRO X_POS, Y_POS, COLOR
     push ax
     push bx
@@ -39,3 +39,29 @@ DRAW_PIXEL MACRO X_POS, Y_POS, COLOR
     pop bx
     pop ax
 ENDM
+
+SetCursor macro row,col	;設定游標位置
+          mov dh,row
+          mov dl,col
+          mov bx,00h
+          mov ah,02h
+          int 10h
+endm
+
+printstr macro string ,color;列印字串
+        push si
+        push ax
+        push bx
+        lea si,string
+        mov ah,0Eh
+        .while byte ptr [si] != '$'
+            mov al,[si]
+            mov bh,00h
+            mov bl,color
+            int 10h
+            inc si
+        .endw
+        pop bx
+        pop ax
+        pop si
+endm
